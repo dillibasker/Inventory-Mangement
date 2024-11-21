6af17f15b11a1fa1bd366c368f6361741f79cbd2
Your Name <your.email@example.com>
2024-11-21 13:56:22 +0530

    first commit

---
+++ Backend/inventory/fire/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
